8c068f27a5c704878cf8f5d196808100a6c1da19
zhiyongli <iamlizhiyong@outlook.com>
2023-04-27 17:28:08 +0800

    Add Stop and Start message handle

--- helloworld/src/main/java/org/springframework/amqp/helloworld/async/ConsumerConfiguration.java
+++ helloworld/src/main/java/org/springframework/amqp/helloworld/async/ConsumerConfiguration.java
@@ -3,8 +3,14 @@ package org.springframework.amqp.helloworld.async;
 import org.springframework.amqp.helloworld.HelloWorldConfiguration;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 public class ConsumerConfiguration extends HelloWorldConfiguration {
@@ -16,6 +22,40 @@ public class ConsumerConfiguration extends HelloWorldConfiguration {
 		container.setQueueNames(this.helloWorldQueueName);
 		container.setMessageListener(new MessageListenerAdapter(new HelloWorldHandler()));
 		return container;
+	}
+
+	@Bean
+	public ConsumerConfiguration.ScheduledProducer scheduledProducer() {
+		return new ConsumerConfiguration.ScheduledProducer();
+	}
+
+	@Bean
+	public BeanPostProcessor postProcessor() {
+		return new ScheduledAnnotationBeanPostProcessor();
+	}
+
+
+	static class ScheduledProducer {
+
+		private final AtomicInteger counter = new AtomicInteger();
+
+		@Autowired
+		private SimpleMessageListenerContainer simpleMessageListenerContainer;
+
+		private int count;
+
+		@Scheduled(fixedRate = 3000)
+		public void stop() {
+			count++;
+			System.out.println("Check count:"+count);
+			if(count>10 && count<20){
+				System.out.println("Stop Consumer");
+				simpleMessageListenerContainer.stop();
+			}else if(count>20){
+				System.out.println("Start Consumer");
+				simpleMessageListenerContainer.start();
+			}
+		}
 	}
 
 }
